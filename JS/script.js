c9a1ebc0756e6f6d551d1208395b5cc50391a8f7
SebDevYogi <sebdevyogi@pm.me>
2021-06-02 17:00:14 +0200

    Nettoyage de code + finalaisation des fonctionnalité de Fetch sur page produit + démarrage code pour ajout du produits au panier (JS)

--- JS/script.js
+++ JS/script.js
@@ -1,63 +1,100 @@
+    // Variables
+    const nameDiv = document.getElementById('name'),
+          descriptionDiv = document.getElementById('description'),
+          priceDiv = document.getElementById('price'),
+          photoDiv = document.getElementById('photo'),
+          qtyDiv = document.getElementById('quantity')
+          cartsDiv = document.getElementById('add_cart');
+
+    let ID = window.location.hash.substr(10);
+    let logTest = "";
+    let furniture = "";
+    let vernis = "";
+    let cartAlert = document.getElementById('cart_alert');
+
+    // Cart
+    let cart = [];
+    // Charger la sélection
+    class Products {
+        async getProducts () {
+            try{
+                fetch('http://localhost:3000/api/furniture')
+                    .then(function(res) {
+                        if (res.ok) {
+                            return res.json();
+                        }
+                    })
+                    .then(data => logTest = data)
+
+                }
+                catch (error) {
+                    console.log(errors);
+                }
+            }
+    }
+    // Afficher la selection
+    class UI {
+
+    }
+    // Stocker la sélection en local
+    class Storage{
+
+    } 
+
+    document.addEventListener("DOMContentLoaded",() => {
+        const ui = new UI();
+        const products = new Products();
+
+        // Charger les produits
+    });
+
     // Charge le script uniquement sur la page Article et lance la fonction de récupération de données API
     if(window.location.pathname == '/product.html') {
         document.addEventListener('DOMContentLoaded',article),
-        document.getElementById('quantity').addEventListener('input', article);
-
-        var ID = window.location.hash.substr(10);
+        qtyDiv.addEventListener('input', function(){
+            priceDiv.innerHTML = (((furniture.price*(qtyDiv.value))/100).toFixed(2))+"€";
+        });
 
         console.log(ID); 
         // Va récupérer les données de l'API, les convertir et les placer ds le DOM
         function article(){
             fetch('http://localhost:3000/api/furniture'+'/'+ID)
-
                 .then(function(res) {
                     if (res.ok) {
                         return res.json();
                     }
                 })
-                .then(function(value) {
-                    document.getElementById('name').innerHTML = value.name;
-                    document.getElementById('description').innerHTML = value.description;
-                    document.getElementById('price').innerHTML = (((value.price*(document.getElementById('quantity').value))/100).toFixed(2))+"€";
-                    document.getElementById('photo').src=value.imageUrl;
-                    document.getElementById('0').innerHTML = value.varnish[0];
-                    document.getElementById('1').innerHTML = value.varnish[1];
-                    document.getElementById('2').innerHTML = value.varnish[2];
-                    document.getElementById('3').innerHTML = value.varnish[3];
+                .then(data => furniture = data)
+                .then (furniture => vernis = furniture.varnish)
+                .then(function() {
+                    nameDiv.innerHTML = furniture.name;
+                    descriptionDiv.innerHTML = furniture.description;
+                    priceDiv.innerHTML = (((furniture.price*(qtyDiv.value))/100).toFixed(2))+"€";
+                    photoDiv.src=furniture.imageUrl;
+                        vernis.forEach(createOption);
+                            function createOption(item, index){
+                                let opt = document.createElement('option');
+                                opt.value = index;
+                                opt.innerHTML += item;
+                                document.getElementById('optionv').appendChild(opt);
+                            }
                 })
-
                 .catch(function(err) {
-
+                    console.log("Attention, quelque chose ne tourne pas rond!!!")
                 })
-        };              
-    }
-
-    let carts = document.getElementById('add_cart');
-    let cartAlert = document.getElementById('cart_alert');
-    let products = [
-        ];
-
+        };
     // Ecouteur sur le bouton Ajouter
-    carts.addEventListener('click', () => {
+    cartsDiv.addEventListener('click', () => {
         cartNumbers();
     });
 
-    for (let i=0; i < carts.length; i ++) {
-        carts[i].addEventListener('click', () => {
-            cartNumbers(products[i]);
+    // Incrémente le cart au fur et a mesure des clics
+    for (let i=0; i < cartsDiv.length; i ++) {
+        cartsDiv[i].addEventListener('click', () => {
+            cartNumbers(cart[i]);
         })
     }
 
-    // Permet d'afficher le nombre de produit ds le cart au chargement de la page
-    onLoadCartNumbers();
-
-    function onLoadCartNumbers() {
-        let productNumbers = localStorage.getItem('cartNumbers');
-
-        if(productNumbers) {
-            cartAlert.textContent = productNumbers;
-        }
-    }
     // Permet d'ajouter +1 ds le cart
     function cartNumbers(product){
         console.log('The product choosed is', product);
@@ -72,12 +109,17 @@
             localStorage.setItem('cartNumbers', 1);
             cartAlert.textContent = 1;
         }
-
-        setItems (product);
-
     }
-
-    function setItems (product) {
-        console.log('Inside of setItems function' +
-        '\n My product is ' + product)
     }
+    // Permet d'afficher le nombre de produit ds le cart au chargement de la page
+    onLoadCartNumbers();
+    function onLoadCartNumbers() {
+        let productNumbers = localStorage.getItem('cartNumbers');
+
+        if(productNumbers) {
+            cartAlert.textContent = productNumbers;
+        }
+    }
+    
+    
+
