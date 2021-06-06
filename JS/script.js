db7f148a0ab6398419be7209d38d75d30bcb6aa2
SebDevYogi <sebdevyogi@pm.me>
2021-06-06 15:41:10 +0200

    Ajout bouton Vider Panier + calculs des totaux

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-
     // Variables
     const nameDiv = document.getElementById('name'),
           descriptionDiv = document.getElementById('description'),
@@ -6,29 +5,22 @@
           photoDiv = document.getElementById('photo'),
           qtyDiv = document.getElementById('quantity'),
           varnishDiv = document.getElementById('optionv'),
-          cartsDiv = document.getElementById('add_cart');
+          cartsDiv = document.getElementById('add_cart')
+          priceSubttDiv = document.getElementById('price_subtt'),
+          tvaDiv = document.getElementById('tva'),
+          priceTtDiv = document.getElementsByClassName('total_price'),
+          deleteBtnDiv = document.getElementsByClassName('cart_buttons'),
+          emptyBtnDiv = document.getElementById('empty_button');
+
 
     let ID = window.location.hash.substr(10);
     let logTest = "";
     let furniture = "";
     var vernis = "";
-    let cartAlert = document.getElementById('cart_alert');
+    let cartAlertDiv = document.getElementById('cart_alert');
 
-    // Cart
-    let cart = [];
-    // Charger la sélection
-    class Products {
-        async getProducts () {
-            try{
-                let result = await fetch('http://localhost:3000/api/furniture')
-                .then(response => response.json())
-                    return result;
-                }
-                catch (error) {
-                    console.log(error);
-                }
-            }
-    }
+    document.addEventListener('DOMContentLoaded',onLoadCartInitialization)
+
     // Charge le script uniquement sur la page Article et lance la fonction de récupération de données API
     console.log(window.location.pathname)
         if(window.location.pathname === '/product.html')
@@ -37,7 +29,8 @@
                 priceDiv.innerHTML = (((furniture.price*(qtyDiv.value))/100).toFixed(2))+"€";
             });
 
-        console.log(ID); 
+        console.log(ID);
+
             // Va récupérer les données de l'API, les convertir et les placer ds le DOM
             function article(){
                 fetch('http://localhost:3000/api/furniture'+'/'+ID)
@@ -66,51 +59,6 @@
                     })
                 };
 
-                // Ecouteur sur le bouton Ajouter
-                cartsDiv.addEventListener('click', () => {
-                    cartNumbers();
-                });
-
-                // Incrémente le cart au fur et a mesure des clics
-                for (let i=0; i < cartsDiv.length; i ++) {
-                    cartsDiv[i].addEventListener('click', () => {
-                        cartNumbers(cart[i]);
-                    })
-                }
-
-                // Permet d'ajouter +1 ds le cart
-                function cartNumbers(product){
-                    let productNumbers = localStorage.getItem('cartNumbers');
-
-                    productNumbers = parseInt(productNumbers);
-                    selectedQty = parseInt(qtyDiv.value);
-
-                if( productNumbers ) {
-                    localStorage.setItem('cartNumbers', productNumbers + selectedQty);
-                    cartAlert.textContent = productNumbers + selectedQty;
-                } else {
-                    localStorage.setItem('cartNumbers', selectedQty);
-                    cartAlert.textContent = selectedQty;
-                    }
-                }
-                // // Permet d'ajouter la selection au cart sous forme d'objet
-                // cartsDiv.addEventListener('click', function() {
-                //     let selectedVarnish = varnishDiv.selectedOptions;
-                //     let localSelector = selectedVarnish[0].innerHTML;
-                //     console.log(selectedVarnish)
-                //     let selected ={
-                //         Id: ID,
-                //         Name: nameDiv.textContent,
-                //         Varnish: selectedVarnish[0].innerHTML,
-                //         Qty: qtyDiv.value,
-                //         Price: priceDiv.textContent,
-                        
-                //     }
-                //     localStorage.setItem(ID+"_"+localSelector, JSON.stringify(selected));
-                // })
-
-                // // Permet d'ajouter la selection au cart sous forme de tableau par selection
-                
                 cartsDiv.addEventListener('click', function() {
                     let selectedVarnish = varnishDiv.selectedOptions;
                     let localSelector = selectedVarnish[0].innerHTML;
@@ -121,46 +69,61 @@
                         Description: descriptionDiv.textContent,
                         Photo: photoDiv.src,
                         Varnish: selectedVarnish[0].innerHTML,
-                        Qty: qtyDiv.value,
-                        Price: (((furniture.price*(qtyDiv.value))/100).toFixed(2)),
+                        Qty: parseInt(qtyDiv.value),
+                        Price: parseInt((((furniture.price*(qtyDiv.value))/100).toFixed(2))),
                         
                     }
                     localArray.push(selection.Id, selection.Name, selection.Description, selection.Photo, selection.Varnish, selection.Qty, selection.Price);
-                    console.table(selection)
+                    console.log(selection)
+                    console.log(typeof(selection.Price))
                     localStorage.setItem(ID+"_"+localSelector, JSON.stringify(localArray))
+
+                    onLoadCartInitialization();
+                    alert('Produit ajouté à votre panier')
                 })
-                console.table(localStorage.getItem(JSON.stringify(ID)))
         }
 
     // Charge le script uniquement sur la page Panier et lance la fonction de récupération de données en stockage loacale
     if(window.location.pathname === '/cart.html') {
-        // let allStorage = [...localStorage];
-        // console.log(allStorage)
-        // allStorage.key.forEach(createOption);
-        //     function createOption(ID, Name, Varnish, Qty, Price){
-        //         let card = document.createElement('div');
-        //         card.className = cart_card;
-        //         card.innerHTML += '<div id="my_selection" class="cart_card"><img src="./images/oak_1.webp" alt="table_design_bois"><div class="product_infos"><div class="product_title"><h3 class="ttl">Cross Table</h3><p class="price_unit"><span class="price_unit">      599.00</span>€</p></div><div class="underline"></div><div class="card_bottom"><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum nisi dolorum sapiente quo exercitationem, inventore cupiditate reiciendis numquam cum repellendus eveniet consequuntur voluptatibus obcaecati rem tenetur quos est deleniti. Quaerat?</p><div class="cart_modifications"><div class="product_numbers"><p class="varnish_cart">Vernis :<br> <strong><span class="varnish_cart">Dark Oak</span></strong></p><p class="qty">Quantité : <br><strong><span class="qty">2</span></strong></p><p class="price_total">Tarif : <br><strong><span class="price_total">1198.00</span>€</strong></p></div><div class="cart_buttons"><input class="cart_delete" type="button" value="Delete"></div></div>/div></div></div>;'
-        //         document.getElementById('cart_list').appendChild(card);
-        //     }
         let myCart = [];
+        
         Object.keys(localStorage).forEach(function(key){
-            console.log(localStorage.getItem(key));
-            let test = JSON.parse(localStorage.getItem(key));
-            console.log(test);
-            console.log(test[4]);
-            myCart.push(key);
-            console.table(myCart)
-            console.log
+            // console.log(localStorage.getItem(key));
+            let cartArray = JSON.parse(localStorage.getItem(key));
+            // console.table(test);
+            myCart.push(cartArray);
+            // console.table(myCart)
             let card = document.createElement('div');
                 card.id = key;
                 card.className = "cart_card";
-                card.innerHTML += '<img src="'+test[3]+'" alt="table_design_bois"><div class="product_infos"><div class="product_title"><h3 class="ttl">' + test[1] + '</h3><p class="price_unit"><span class="price_unit">'+test[6]/test[5] +'</span>€</p></div><div class="underline"></div><div class="card_bottom"><p>'+test[2]+'</p><div class="cart_modifications"><div class="product_numbers"><p class="varnish_cart">Vernis :<br> <strong><span class="varnish_cart_choosed">'+test[4]+'</span></strong></p><p class="qty">Quantité : <br><strong><span class="qty">'+test[5]+'</span></strong></p><p class="price_total">Tarif : <br><strong><span class="price_total">'+test[6]+'</span>€</strong></p></div><div class="cart_buttons"><input class="cart_delete" type="button" value="Delete"></div></div><div class="underline"></div></div></div>'
+                card.innerHTML += '<img src="'+cartArray[3]+'" alt="table_design_bois"><div class="product_infos"><div class="product_title"><h3 class="ttl">' + cartArray[1] + '</h3><p class="price_unit"><span class="price_unit">'+(cartArray[6]/cartArray[5]).toFixed(2) +'</span>€</p></div><div class="underline"></div><div class="card_bottom"><p>'+cartArray[2]+'</p><div class="cart_modifications"><div class="product_numbers"><p class="varnish_cart">Vernis :<br> <strong><span class="varnish_cart_choosed">'+cartArray[4]+'</span></strong></p><p class="qty">Quantité : <br><strong><span class="qty">'+cartArray[5]+'</span></strong></p><p class="price_total">Tarif : <br><strong><span class="price_total">'+cartArray[6]+'</span>€</strong></p></div><div class="cart_buttons"><input class="cart_delete" type="button" value="Retirer"></div></div><div class="underline"></div></div></div>'
 
-                document.getElementById('cart_list').appendChild(card);
-            
+                document.getElementById('cart_list').appendChild(card); 
         })
-        
+        let sumMyCart = (myCart) => {
+
+            let newArray = [];
+            myCart.forEach(sub => {
+                sub.forEach((num, index) => {
+                    if(newArray[index]) {
+                        newArray[index] += num;
+                    } else {
+                        newArray[index] = num;
+                    }
+                });
+            });
+            return (newArray);
+        }
+        let subtts = sumMyCart(myCart);
+        priceSubttDiv.textContent = (subtts[6]).toFixed(2);
+        tva.textContent = ((subtts[6]/120)*20).toFixed(2);
+        priceTtDiv.textContent = (subtts[6]).toFixed(2);
+
+        emptyBtnDiv.addEventListener('click', function() {
+            localStorage.clear();
+            location.reload();
+        });
+
     }
 
 
@@ -176,14 +139,33 @@
 
 
     // Permet d'afficher le nombre de produit ds le cart au chargement de la page
-    onLoadCartNumbers();
-    function onLoadCartNumbers() {
-        let productNumbers = localStorage.getItem('cartNumbers');
-
-        if(productNumbers) {
-            cartAlert.textContent = productNumbers;
+    function onLoadCartInitialization(){        
+        let myCart = [];
+        Object.keys(localStorage).forEach(function(key){
+            let cartArray = JSON.parse(localStorage.getItem(key));
+            myCart.push(cartArray);
+        })
+        let sumMyCart = (myCart) => {
+            let newArray = [];
+            myCart.forEach(sub => {
+                sub.forEach((num, index) => {
+                    if(newArray[index]) {
+                        newArray[index] += num;
+                    } else {
+                        newArray[index] = num;
+                    }
+                });
+            });
+            return (newArray);
         }
-    }
+        console.table(myCart);
+        let cart = sumMyCart(myCart);
+        console.table(cart);
+            cartAlertDiv.textContent = cart[5];
+            console.log(cart[5])
+            console.log(typeof(cartAlertDiv))
+        }
+    
 
     
     
